bc496b1bbb092821b3b24cb8ee8a395cda3095af
Matthieu LEFEVRE <matth.lfvr@gmail.com>
2023-02-15 18:53:03 +0100

    1st inversion ok

--- main.cpp
+++ main.cpp
@@ -24,6 +24,14 @@ bool contains(std::vector<std::string> vec, std::string str)
 	return false;
 }
 
+bool contains(std::vector<int> vec, int num)
+{
+	for (int i = 0; i < vec.size(); i++)
+		if (vec[i]%12 == num)
+			return true;
+	return false;
+}
+
 int findSmallest(std::vector<int> vec)
 {
 	int smallest = vec[0];
@@ -36,7 +44,7 @@ int findSmallest(std::vector<int> vec)
 std::string numToName(int num)
 {
 	std::string notes[] = { "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B" };
-	return notes[num % 12].append(std::to_string(num / 12));
+	return notes[num % 12];
 }
 
 std::string tickToPulse(int tick)
@@ -56,7 +64,21 @@ std::string tickToPulse(int tick)
 void computeChord()
 {
 	int root = findSmallest(notes);
-	std::string chord(1,numToName(root).front());
+	std::string chord(numToName(root));
+	// find 3rd
+	bool minor = false;
+	if (contains(notes, (root+3)%12))
+		minor = true;
+	// 1st inversion
+	if (minor && contains(notes, (root+8)%12)) {
+		root = (root+8)%12;
+		chord.append("/");
+		chord.append(numToName(root));
+	} else if (!minor && !contains(notes, (root+5)%12) && contains(notes, (root+9)%12)) {
+		root = (root+9)%12;
+		chord.append("/");
+		chord.append(numToName(root));
+	}
 	for (int i = 0; i < notes.size(); i++)
 		chord.append(extaNotes[(notes[i] - root) % 12]);
 	if (!contains(chords, chord))
@@ -79,20 +101,21 @@ int main(int argc, char** argv)
 	}
 	for (int i = 0; i < midifile[0].size(); i++)
 	{
+		int num = midifile[0][i].getKeyNumber();
 		// si les notes s'enlevent en même temps, il ne faut pas compute
 		if (lastTick != midifile[0][i].tick && currentNoteOn > 2)
 			computeChord();
 		if (midifile[0][i].isTempo())
 			std::cout << "BPM : " << midifile[0][i].getTempoBPM() << std::endl;
 		if (midifile[0][i].isNoteOn()) {
-			std::cout << "Note On : " << numToName(midifile[0][i].getKeyNumber()) << " at the " << tickToPulse(midifile[0][i].tick) << std::endl;
+			std::cout << "Note On : " << numToName(num).append(std::to_string(num / 12)) << " at the " << tickToPulse(midifile[0][i].tick) << std::endl;
 			currentNoteOn++;
 			notes.push_back(midifile[0][i].getKeyNumber());
 		}
 		if (midifile[0][i].isNoteOff()) {
-			std::cout << "Note Off : " << numToName(midifile[0][i].getKeyNumber()) << " at the " << tickToPulse(midifile[0][i].tick) << std::endl;
+			std::cout << "Note Off : " << numToName(num).append(std::to_string(num / 12)) << " at the " << tickToPulse(midifile[0][i].tick) << std::endl;
 			currentNoteOn--;
-			notes.erase(std::remove(notes.begin(), notes.end(), midifile[0][i].getKeyNumber()), notes.end());
+			notes.erase(std::remove(notes.begin(), notes.end(), num), notes.end());
 		}
 		lastTick = midifile[0][i].tick;
 	}
