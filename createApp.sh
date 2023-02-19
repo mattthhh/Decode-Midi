74b7d58e5f4d94e8fcaeee8bc1fbeb8d19b320d0
Matthieu LEFEVRE <matth.lfvr@gmail.com>
2023-02-19 19:43:47 +0100

    MacOs installer configure

---
+++ createApp.sh
@@ -0,0 +1,2 @@
+cp ./DecodeMidi /Applications/DecodeMidi.app/Contents/MacOS/DecodeMidi
+create-dmg --icon "/Applications/DecodeMidi.app" 0 0 --app-drop-link 0 200 DecodeMidi.dmg /Applications/DecodeMidi.app
