ea3e9111a3483b92b6c0dbb5a759ebb4399e36d9
Matthieu LEFEVRE <matth.lfvr@gmail.com>
2023-03-01 10:10:12 +0100

    found background color of the window

--- display.cpp
+++ display.cpp
@@ -12,6 +12,9 @@ m_label("Click or drop a MIDI file here"),
 m_labelOutput("Output log"),
 m_textView()
 {
+	// print the background color of the window
+	std::cout << get_style_context()->get_background_color(Gtk::STATE_FLAG_NORMAL).to_string() << std::endl;
+
 	set_title("Decode MIDI");
 	set_default_size(700, 400);
 	set_resizable(false);
