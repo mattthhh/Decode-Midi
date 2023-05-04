52b0ae97d3abd6fb2dd51578a15d0aa2bf16a02b
Matthieu LEFEVRE <matth.lfvr@gmail.com>
2023-05-05 01:43:11 +0200

    Python code okay but model broken

--- createData.sh
+++ createData.sh
@@ -3,10 +3,8 @@ ls songs | while read category
 do
 	ls songs/$category | while read song
 	do
-		touch data/$category-$song-song.txt
-		touch data/$category-$song-cat.txt
 		echo "--- Doing $category-$song ---"
-		./DecodeMidi songs/$category/$song >> data/$category-$song.txt
+		./DecodeMidi songs/$category/$song >> data/$category-$song-song.txt
 		echo $category >> data/$category-$song-cat.txt
 	done
 done
--- neural.py
+++ neural.py
@@ -1,6 +1,10 @@
 import glob
 from sklearn.model_selection import train_test_split
+import numpy as np
+import tensorflow as tf
+from sklearn.preprocessing import LabelEncoder
 
+# import data
 data_path = "data/*song.txt"
 files = glob.glob(data_path)
 data = []
@@ -15,5 +19,44 @@ for file in files:
     with open(file, "r") as f:
         labels.append(f.read())
 
-X_train_val, X_test, y_train_val, y_test = train_test_split(data, labels, test_size=0.2, random_state=42, stratify=labels)
-X_train, X_val, y_train, y_val = train_test_split(X_train_val, y_train_val, test_size=0.2, random_state=42, stratify=y_train_val)
+# lebels encoding with integers
+label_encoder = LabelEncoder()
+label_encoder.fit(labels)
+labels = label_encoder.transform(labels)
+
+# tokenization
+tokenizer = tf.keras.preprocessing.text.Tokenizer()
+tokenizer.fit_on_texts(data)
+sequences = tokenizer.texts_to_sequences(data)
+max_sequence_length = max([len(seq) for seq in sequences])
+padded_sequences = tf.keras.preprocessing.sequence.pad_sequences(sequences, maxlen=max_sequence_length, padding="post")
+
+# split data 80% train, 20% test
+split_index = int(len(padded_sequences) * 0.8)
+train_sequences = padded_sequences[:split_index]
+train_labels = labels[:split_index]
+test_sequences = padded_sequences[split_index:]
+test_labels = labels[split_index:]
+
+print(train_sequences.shape)
+print(test_sequences.shape)
+
+print(data[0])
+print(train_sequences[0])
+print(data[1])
+print(train_sequences[1])
+
+# CNN Model
+model = tf.keras.Sequential([
+    tf.keras.layers.Embedding(input_dim=len(tokenizer.word_index) + 1, output_dim=32, input_length=max_sequence_length),
+    tf.keras.layers.Conv1D(32, 5, activation="relu"),
+    tf.keras.layers.GlobalMaxPooling1D(),
+    tf.keras.layers.Dense(32, activation="relu"),
+    tf.keras.layers.Dense(1, activation="sigmoid")
+])
+
+model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
+model.fit(train_sequences, train_labels, epochs=10, batch_size=32, validation_data=(test_sequences, test_labels))
+test_loss, test_acc = model.evaluate(test_sequences, test_labels)
+print("Test Loss: ", test_loss)
+print("Test Accuracy: ", test_acc)
